ec868e0e2a9bcbda6626fd95d583085c037c28b9
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-07-08 12:50:19 +0600

    practice auth

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFyeoncGu8KEoNR32Qi0hdgS8T51GYp7A",
+  authDomain: "auth-again-ea128.firebaseapp.com",
+  projectId: "auth-again-ea128",
+  storageBucket: "auth-again-ea128.appspot.com",
+  messagingSenderId: "852605694316",
+  appId: "1:852605694316:web:547c6229a89fd02472c349",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
