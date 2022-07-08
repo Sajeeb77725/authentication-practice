2d73636e7260c558fdcc0c0916aec32d8fb0e588
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-07-08 13:22:46 +0600

    second one

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import "./App.css";
 import app from "./firebase.init";
 
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSingIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
 
@@ -23,6 +27,9 @@ function App() {
   return (
     <div className="App">
       <button onClick={handleGoogleSingIn}>Google Sing in</button>
+      <h1>Name: {user.displayName}</h1>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
